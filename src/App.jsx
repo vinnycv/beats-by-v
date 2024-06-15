65314dae513b79c954be53403a12e6b4480f453f
Vince Vizza <vinny@vizza.dev>
2024-06-15 12:55:10 -0500

    Create file for audio links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
-// "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/47[kb]flarzbass.wav.mp3"
-
-const soundBank1 = [{
-  padId: "Q",
-  name: "Heater 1",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-}, {
-  padId: "W",
-  name: "Heater 2",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-}, {
-  padId: "E",
-  name: "Heater 3",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-}, {
-  padId: "A",
-  name: "Heater 4",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-}, {
-  padId: "S",
-  name: "Clap",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-}, {
-  padId: "D",
-  name: "Open H-H",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-}, {
-  padId: "Z",
-  name: "Close H-H",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-}, {
-  padId: "X",
-  name: "Kick-n-hat",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-}, {
-  padId: "C",
-  name: "Kick",
-  audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-},];
-
-const soundBank2 = [{
-  padId: "Q",
-  name: "Jub",
-  audioUrl: "https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Cheesy%20Lo-Fi%20Sound%20Effects/10[kb]Jub.wav.mp3"
-}, {
-  padId: "W",
-  name: "Rave",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/15[kb]classicrave.wav.mp3"
-}, {
-  padId: "E",
-  name: "Buzzsaw",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/61[kb]buzzsaw-bass-synth.wav.mp3"
-}, {
-  padId: "A",
-  name: "Round",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/51[kb]roundresonote.wav.mp3"
-}, {
-  padId: "S",
-  name: "Long round",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/115[kb]round-2.wav.mp3"
-}, {
-  padId: "D",
-  name: "Bass",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/29[kb]final-round.wav.mp3"
-}, {
-  padId: "Z",
-  name: "Burp",
-  audioUrl: "https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Cheesy%20Lo-Fi%20Sound%20Effects/9[kb]Belch.wav.mp3"
-}, {
-  padId: "X",
-  name: "Hard kick",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/168[kb]hard-kick-synth.wav.mp3"
-}, {
-  padId: "C",
-  name: "Laser",
-  audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/89[kb]resybass.wav.mp3"
-},];
+import { soundBank1, soundBank2 } from './sound-data.js'
 
 // const soundKit = ["Heater kit", "Piano kit"];
 
@@ -86,11 +9,11 @@ const off = [{padId: "", name: "Power off", audioUrl: ""}];
 
 function App() {
   
-  const [power, setPower] = React.useState(false);
-  const [bank, setBank] = React.useState(off);
-  const [title, setTitle] = React.useState("Turn on power");
-  const [display, setDisplay] = React.useState("Power off");
-  const [volume, setVolume] = React.useState(0.5);
+  const [power, setPower] = useState(false);
+  const [bank, setBank] = useState(off);
+  const [title, setTitle] = useState("Turn on power");
+  const [display, setDisplay] = useState("Power off");
+  const [volume, setVolume] = useState(0.5);
     
   function onPowerToggle() {
     let slider = document.getElementsByClassName('Power')[0];
@@ -258,8 +181,5 @@ function Toggle({title, kit, onToggle}) {
     </>
   )
 }
-
-// .render is deprecated!!!
-// ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App
---
+++ src/sound-data.js
@@ -0,0 +1,77 @@
+// "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/47[kb]flarzbass.wav.mp3"
+
+export const soundBank1 = [{
+    padId: "Q",
+    name: "Heater 1",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  }, {
+    padId: "W",
+    name: "Heater 2",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  }, {
+    padId: "E",
+    name: "Heater 3",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  }, {
+    padId: "A",
+    name: "Heater 4",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  }, {
+    padId: "S",
+    name: "Clap",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  }, {
+    padId: "D",
+    name: "Open H-H",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  }, {
+    padId: "Z",
+    name: "Close H-H",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }, {
+    padId: "X",
+    name: "Kick-n-hat",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  }, {
+    padId: "C",
+    name: "Kick",
+    audioUrl: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  }];
+  
+export const soundBank2 = [{
+    padId: "Q",
+    name: "Jub",
+    audioUrl: "https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Cheesy%20Lo-Fi%20Sound%20Effects/10[kb]Jub.wav.mp3"
+  }, {
+    padId: "W",
+    name: "Rave",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/15[kb]classicrave.wav.mp3"
+  }, {
+    padId: "E",
+    name: "Buzzsaw",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/61[kb]buzzsaw-bass-synth.wav.mp3"
+  }, {
+    padId: "A",
+    name: "Round",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/51[kb]roundresonote.wav.mp3"
+  }, {
+    padId: "S",
+    name: "Long round",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/115[kb]round-2.wav.mp3"
+  }, {
+    padId: "D",
+    name: "Bass",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/29[kb]final-round.wav.mp3"
+  }, {
+    padId: "Z",
+    name: "Burp",
+    audioUrl: "https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Cheesy%20Lo-Fi%20Sound%20Effects/9[kb]Belch.wav.mp3"
+  }, {
+    padId: "X",
+    name: "Hard kick",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/168[kb]hard-kick-synth.wav.mp3"
+  }, {
+    padId: "C",
+    name: "Laser",
+    audioUrl: "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Synth/89[kb]resybass.wav.mp3"
+  }];
